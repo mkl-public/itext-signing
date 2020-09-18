33f91039abfd6744eced53763fd5a07e9ce73782
mkl <klink@csi.com>
2020-09-18 19:34:28 +0200

    New .Net itext-signing solution with pkcs11.Net and pkcs11.Net-test projects; first PKCS#11 signing test
    pkcs11.Net is the .Net project providing an IExternalSignature implementation for signing via PKCS#11.
    pkcs11.Net-test is the .Net project focusing on testing PKCS#11 Interop in general and signing via PKCS#11 using the pkcs11.Net IExternalSignature implementation in particular.
    
    The IExternalSignature implementation Pkcs11Signature handles the PKCS#11 related initialization in excess to the actual signing.
    TestPkcs11Access tests accessibility of the SoftHSM2 backend.
    TestSignSimple tests the creation of a simple PDF signature via the SoftHSM2 backend via a Pkcs11Signature instance.

---
+++ pkcs11.Net-test/TestSignSimple.cs
@@ -0,0 +1,27 @@
+﻿using iText.Kernel.Pdf;
+using iText.Signatures;
+using NUnit.Framework;
+using System;
+using System.IO;
+using static iText.Signatures.PdfSigner;
+
+namespace itext.signing.pkcs11_Net
+{
+    class TestSignSimple
+    {
+        [Test]
+        public void TestPkcs11SignSimple()
+        {
+            string testFileName = @"..\..\..\resources\circles.pdf";
+
+            using (Pkcs11Signature signature = new Pkcs11Signature(@"d:\Program Files\SoftHSM2\lib\softhsm2-x64.dll", 171137967).Select(null, "5678").SetHashAlgorithm("SHA256"))
+            using (PdfReader pdfReader = new PdfReader(testFileName))
+            using (FileStream result = File.Create("circles-pkcs11-signed-simple.pdf"))
+            {
+                PdfSigner pdfSigner = new PdfSigner(pdfReader, result, new StampingProperties().UseAppendMode());
+
+                pdfSigner.SignDetached(signature, signature.GetChain(), null, null, null, 0, CryptoStandard.CMS);
+            }
+        }
+    }
+}
