77ab9dd674423bf673104f5d4fb81a05957f4dd6
mkl <klink@csi.com>
2021-01-07 17:51:51 +0100

    Added signing test hardcoded for an Utimaco Simulator

---
+++ pkcs11/src/test/java/mkl/itext/signing/pkcs11/utimaco/TestSignSimple.java
@@ -0,0 +1,39 @@
+package mkl.itext.signing.pkcs11.utimaco;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import org.junit.jupiter.api.Test;
+
+import mkl.itext.signing.pkcs11.BaseSignSimple;
+
+/**
+ * <p>
+ * This test class signs a PDF file using {@link BaseSignSimple}
+ * with variables set to access an Utimaco Simulator as
+ * configured and initialized on the original development
+ * machine.
+ * </p>
+ * <p>
+ * Please remember to set the <code>CS_PKCS11_R2_CFG</code>
+ * environment variable to point to the Utimaco configuration
+ * file <code>cs_pkcs11_R2.cfg</code>.
+ * </p>
+ * 
+ * @author mkl
+ */
+class TestSignSimple extends BaseSignSimple {
+
+    @Test
+    void test() throws IOException, GeneralSecurityException {
+        config = "--name = Utimaco\n"
+                + "library = d:/Program Files/Utimaco/CryptoServer/Lib/cs_pkcs11_R2.dll\n"
+                + "slot = 0\n";
+        alias = null;
+        pin = "5678".toCharArray();
+        result = new File(RESULT_FOLDER, "circles-pkcs11-signed-simple-utimaco.pdf");
+        testSignSimple();
+    }
+
+}
---
+++ pkcs11/src/test/java/mkl/itext/signing/pkcs11/utimaco/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * <p>
+ * The tests in this package make use of the base tests
+ * and set variables to access an Utimaco Simulator as
+ * configured and initialized on the original development
+ * machine. The values are easy to identify and exchange,
+ * though.
+ * </p>
+ * <p>
+ * Please remember to set the <code>CS_PKCS11_R2_CFG</code>
+ * environment variable to point to the Utimaco configuration
+ * file <code>cs_pkcs11_R2.cfg</code>.
+ * </p>
+ */
+package mkl.itext.signing.pkcs11.utimaco;
