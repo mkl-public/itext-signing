1344e4baa8da5e1d58414a83072a87ae968784d7
mkl <klink@csi.com>
2021-01-08 16:16:17 +0100

    Device specific PKCS11 tests
    Prepared PKCS11 tests for device specific tests
    Added signing test hardcoded for a SoftHSM simulator
    Added signing test hardcoded for an Utimaco Simulator

--- pkcs11-jdk8/src/test/java/mkl/itext/signing/pkcs11/BaseSignSimple.java
+++ pkcs11-jdk8/src/test/java/mkl/itext/signing/pkcs11/BaseSignSimple.java
@@ -10,7 +10,6 @@ import java.security.Security;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.StampingProperties;
@@ -20,10 +19,16 @@ import com.itextpdf.signatures.PdfSigner;
 import com.itextpdf.signatures.PdfSigner.CryptoStandard;
 
 /**
+ * This class provides a simple test routine that signs a PDF
+ * using a {@link Pkcs11Signature}. It furthermore houses variables
+ * for the PKCS11 configuration, alias, PIN, and the result file name.
+ * It is meant to serve as a base class for actual unit test classes
+ * which first set those variables and then call the test routine.
+ * 
  * @author mkl
  */
-class TestSignSimple {
-    final static File RESULT_FOLDER = new File("target/test-outputs", "signature");
+public class BaseSignSimple {
+    public final static File RESULT_FOLDER = new File("target/test-outputs", "signature");
 
     @BeforeAll
     public static void setUpBeforeClass() throws Exception {
@@ -32,20 +37,22 @@ class TestSignSimple {
         Security.addProvider(provider);
     }
 
-    @Test
-    void testSignSimple() throws IOException, GeneralSecurityException {
-        String config = TestEnvironment.getPkcs11Config();
+    protected void testSignSimple() throws IOException, GeneralSecurityException {
         Pkcs11Signature signature = (config.startsWith("--") ? new Pkcs11Signature(config) : new Pkcs11Signature(new File(config)))
-                .select(null, "5678".toCharArray()).setHashAlgorithm("SHA256");
+                .select(alias, pin).setHashAlgorithm("SHA256");
 
         try (   InputStream resource = getClass().getResourceAsStream("/circles.pdf");
                 PdfReader pdfReader = new PdfReader(resource);
-                OutputStream result = new FileOutputStream(new File(RESULT_FOLDER, "circles-pkcs11-signed-simple.pdf"))) {
-            PdfSigner pdfSigner = new PdfSigner(pdfReader, result, new StampingProperties().useAppendMode());
+                OutputStream resultStream = new FileOutputStream(result)    ) {
+            PdfSigner pdfSigner = new PdfSigner(pdfReader, resultStream, new StampingProperties().useAppendMode());
 
             IExternalDigest externalDigest = new BouncyCastleDigest();
             pdfSigner.signDetached(externalDigest , signature, signature.getChain(), null, null, null, 0, CryptoStandard.CMS);
         }
     }
 
+    protected String config;
+    protected String alias;
+    protected char[] pin;
+    protected File result;
 }
