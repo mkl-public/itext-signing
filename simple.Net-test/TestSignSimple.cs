1bc5178f87bde1036343a31a9256669a79fe885d
mkl <klink@csi.com>
2021-07-13 11:46:31 +0200

    New .Net simple.Net and simple.Net-test projects
    simple.Net is the .Net project focusing on providing implementations for simple signing, e.g. using generic X509Certificate2 instances with private keys.
    simple.Net-test is the associated .Net test project focusing on tests for simple signing using simple.Net or iText internal implementations.
    
    X509Certificate2Signature allows signing a PDF with iText 7 using a generic X509Certificate2 instance with a private key. It is a port of the iText 5 for .Net class with the same name, extended to also support CNG keys.
    
    TestSignSimple tests the X509Certificate2Signature.
    TestSignBCSimple tests the iText PrivateKeySignature.

---
+++ simple.Net-test/TestSignSimple.cs
@@ -0,0 +1,40 @@
+﻿using iText.Kernel.Pdf;
+using iText.Signatures;
+using NUnit.Framework;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.X509;
+using System;
+using System.IO;
+using SystemCertificates = System.Security.Cryptography.X509Certificates;
+
+namespace itext.signing.simple_Net
+{
+    class TestSignSimple
+    {
+        [Test]
+        public void testSignSimpleRsa()
+        {
+            string testFileName = @"..\..\..\resources\circles.pdf";
+            string storePath = @"..\..\..\..\simple\keystore\test1234.p12";
+            string storePass = "test1234";
+            string storeAlias = "RSAkey";
+
+            SystemCertificates.X509Certificate2 certificate = new SystemCertificates.X509Certificate2(storePath, storePass);
+            Assert.AreEqual(storeAlias.ToLower(), certificate.FriendlyName?.ToLower(), "Unexpected key alias; overhaul PKCS12 loading.");
+
+            X509Certificate bcCertificate = new X509Certificate(X509CertificateStructure.GetInstance(certificate.RawData));
+            X509Certificate[] chain = { bcCertificate };
+
+            X509Certificate2Signature signature = new X509Certificate2Signature(certificate, "SHA384");
+
+            using (PdfReader pdfReader = new PdfReader(testFileName))
+            using (FileStream result = File.Create("circles-RSA-signed-simple.pdf"))
+            {
+                PdfSigner pdfSigner = new PdfSigner(pdfReader, result, new StampingProperties().UseAppendMode());
+                ITSAClient tsaClient = null;
+
+                pdfSigner.SignDetached(signature, chain, null, null, tsaClient, 0, PdfSigner.CryptoStandard.CMS);
+            }
+        }
+    }
+}
