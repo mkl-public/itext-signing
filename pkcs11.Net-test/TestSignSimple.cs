67dc4efd5158312f1881a3c9baae8bf7df6a96aa
mkl <klink@csi.com>
2021-09-08 17:14:36 +0200

    Made .NET test PKCS#11 parameters generic again

--- pkcs11.Net-test/TestSignSimple.cs
+++ pkcs11.Net-test/TestSignSimple.cs
@@ -14,7 +14,7 @@ namespace itext.signing.pkcs11_Net
         {
             string testFileName = @"..\..\..\resources\circles.pdf";
 
-            using (Pkcs11Signature signature = new Pkcs11Signature(@"c:\Program Files\Entrust\SigningClient\P11SigningClient64.dll", 1).Select(null, "CN=Entrust Limited,OU=ECS,O=Entrust Limited,L=Kanata,ST=Ontario,C=CA", "1234").SetHashAlgorithm("SHA256"))
+            using (Pkcs11Signature signature = new Pkcs11Signature(@"PKCS11LIBRARY", 1).Select("KEYALIAS", "CERTLABEL", "1234").SetHashAlgorithm("SHA256"))
             using (PdfReader pdfReader = new PdfReader(testFileName))
             using (FileStream result = File.Create("circles-pkcs11-signed-simple.pdf"))
             {
