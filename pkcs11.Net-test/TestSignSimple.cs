f57034545563fd4409a3db7bd207cd5498d3b660
mkl <klink@csi.com>
2021-09-20 17:04:40 +0200

    Added Utimaco initialization to .NET PKCS#11 test

--- pkcs11.Net-test/TestSignSimple.cs
+++ pkcs11.Net-test/TestSignSimple.cs
@@ -15,6 +15,9 @@ namespace itext.signing.pkcs11_Net
             string testFileName = @"..\..\..\resources\circles.pdf";
 
             using (Pkcs11Signature signature = new Pkcs11Signature(@"PKCS11LIBRARY", 1).Select("KEYALIAS", "CERTLABEL", "1234").SetHashAlgorithm("SHA256"))
+            // Utimaco
+//            using (Pkcs11Signature signature = new Pkcs11Signature(@"d:/Program Files/Utimaco/CryptoServer/Lib/cs_pkcs11_R2.dll", 0)
+//                .Select(null, null, "5678").SetHashAlgorithm("SHA256"))
             using (PdfReader pdfReader = new PdfReader(testFileName))
             using (FileStream result = File.Create("circles-pkcs11-signed-simple.pdf"))
             {
